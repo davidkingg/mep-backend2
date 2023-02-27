9ab169e964cca6624f03bba49f0848ff52166973
davidkingg <davidoluseje@gmail.com>
2023-02-27 23:11:21 +0100

    wsgi fix4

--- seed/urls.py
+++ seed/urls.py
@@ -16,4 +16,4 @@ urlpatterns = [
     # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += [re_path(r'^.*', TemplateView.as_view())]
+#urlpatterns += [re_path(r'^.*', TemplateView.as_view())]
